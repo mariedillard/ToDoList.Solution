8db5dabe38ac5f60ec7c6c4fa8ef3cce2171a2d7
Marie Dillard <mariedillard@protonmail.com>
2019-02-20 16:03:52 -0800

    functioning unstyled to do list!

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller 
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View(0);
+        }
+    }
+}
---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller 
+    {
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/items")]
+        public ActionResult Create(string description)
+        {
+            Item myItem = new Item(description);
+            return RedirectToAction("Index");
+        }
+        
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll() 
+        {
+            Item.ClearAll();
+            return View();
+        }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Show(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
+        }
+    }
+}
